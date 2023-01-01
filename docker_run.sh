858564a357945a95382d12958fab39f2cf896c3b
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-01-01 15:01:30 -0500

    Updating path 2

--- docker_run.sh
+++ docker_run.sh
@@ -5,4 +5,4 @@
 # Builds container
 sudo docker build -t powershell-ntlm Docker_files/PasswordLessSSH-Container
 # Runs the container
-sudo docker run -it --mount type=bind,src=$(pwd),dst=/tmp2 --mount type=bind,src=$(~/.ssh/),dst=/ssh powershell-ntlm /tmp2/windows_ssh.ps1
+sudo docker run -it --mount type=bind,src=$(pwd),dst=/tmp2 --mount type=bind,src=$HOME/.ssh,dst=/ssh powershell-ntlm /tmp2/windows_ssh.ps1
